ab774ab98d13c987867bf1a56b6b7515ce9ab938
liangguiping <liangguiping@tuandai.com>
2018-08-14 16:21:15 +0800

    hello world

---
+++ hello-world/src/hello.kt
@@ -0,0 +1,11 @@
+
+/**
+ * @AUTHOR lgp
+ * @DATE 2018/8/14 15:50
+ * @DESCRIPTION
+ *
+ **/
+
+fun main(args: Array<String>) {    // 包级可见的函数，接受一个字符串数组作为参数
+    println("Hello World!")         // 分号可以省略
+}
