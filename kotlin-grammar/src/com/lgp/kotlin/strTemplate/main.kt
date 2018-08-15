71c006787a74f865fafbffdb05ccd4f2a1cdb932
liangguiping <liangguiping@tuandai.com>
2018-08-15 10:05:42 +0800

    kotlin 字符串模板的语法

--- kotlin-grammar/src/com/lgp/kotlin/strTemplate/main.kt
+++ kotlin-grammar/src/com/lgp/kotlin/strTemplate/main.kt
@@ -8,6 +8,21 @@ package com.lgp.kotlin.strTemplate
  *
  **/
 fun main(args: Array<String>) {
+
+    val i = 10
+    val s = "i = $i" // 求值结果为 "i = 10"
+    println(s)
+
+    val ss = "ssssss"
+    val sstr = "$ss.length is ${ss.length}" // 求值结果为 ssssss.length is 6
+    println(sstr)
+
+// 使用$ 字符（它不支持反斜杠转义）
+    val price = """
+    ${'$'}9.99
+    """
+    println(price)  // 求值结果为 $9.99
+
 //    $ 表示一个变量名或者变量值
     var a = 1
     println(a)
@@ -20,4 +35,32 @@ fun main(args: Array<String>) {
     val s2 = "${s1.replace("is", "was")}, but now is $a"
     println(s2)
 
+//    String 是不可变的。方括号 [] 语法可以很方便的获取字符串中的某个字符，也可以通过 for 循环来遍历
+    val str = "string"
+
+    for (c in str) {
+        print(c)
+        print(str[1])
+    }
+    println()
+
+    val text = """
+    |多行字符串
+     多行字符串
+    """
+    println(text)   // 输出有一些前置空格
+
+    val text1 = """
+    |多行字符串
+     多行字符串
+    """.trim()
+    println(text1)   // 输出有|
+
+//    可以通过 trimMargin() 方法来删除多余的空白,注意使用这个方法前面必须有|
+//    但你可以选择其他字符并作为参数传入，比如 trimMargin(">")。
+    val trimText = """
+    |多行字符串
+    多行字符串
+    """.trimMargin()
+    println(trimText)// 输出无|
 }
--- kotlin-grammar/src/com/lgp/kotlin/varAndVal/main.kt
+++ kotlin-grammar/src/com/lgp/kotlin/varAndVal/main.kt
@@ -38,8 +38,90 @@ fun main(args: Array<String>) {
         // 在进行过 null 值检查之后, x 和 y 的类型会被自动转换为非 null 变量
         println(x * y)
     }
+
+//三个等号 === 表示比较对象地址，两个 == 表示比较两个值大小
+    val a: Int = 10000
+    println(a === a) // true，值相等，对象地址相等
+
+    //经过了装箱，创建了两个不同的对象
+    val boxedA: Int? = a
+    val anotherBoxedA: Int? = a
+
+    //虽然经过了装箱，但是值是相等的，都是10000
+    println(boxedA === anotherBoxedA) //  false，值相等，对象地址不一样
+    println(boxedA == anotherBoxedA) // true，值相等
+
+//     小类不能转大类,大类不能转小类，要用方法转
+//     每种数据类型都有下面的这些方法，可以转化为其它的类型：
+//    toByte(): Byte
+//    toShort(): Short
+//    toInt(): Int
+//    toLong(): Long
+//    toFloat(): Float
+//    toDouble(): Double
+//    toChar(): Char
+
+//    val b: Byte = 1 // OK, 字面值是静态检测的
+//    val i: Int = b // 错误
+    val b: Byte = 1 // OK,
+    val b1: Int = b.toInt() // OK
+    val c: Int = 1 // OK,
+    val c1: Byte = c.toByte() // OK
+//自动转换时，小能转大
+    val l: Long = 0L + 3 // Long + Int => Long
+//    val l1: Int = 3 + 1L // Long + Int => Long
+
+//    shl(bits) – 左移位 (Java’s <<)
+//    shr(bits) – 右移位 (Java’s >>)
+//    ushr(bits) – 无符号右移位 (Java’s >>>)
+//    and(bits) – 与
+//    or(bits) – 或
+//    xor(bits) – 异或
+//    inv() – 反向
+    val d: Int = 10
+    println(d.shl(1))
+    println(d.shr(1))
+    println(d.ushr(1))
+    println(d.and(1))
+    println(d.or(1))
+    println(d.xor(1))
+    println(d.inv())
+//和 Java 不一样，Kotlin 中的 Char 不能直接和数字操作,Char 必需是单引号 ' 包含起来的
+//    ""里面是string
+    decimalDigitValue('0')
+
+//            || – 短路逻辑或
+//            && – 短路逻辑与
+//            ! - 逻辑非
+    println(true || false)
+    println(true && false)
+    println(!false)
+
+//    数组用类 Array 实现，并且还有一个 size 属性及 get 和 set 方法，由于使用 [] 重载了 get 和 set 方法
+//    除了类Array，还有ByteArray, ShortArray, IntArray，用来表示各个类型的数组
+    //[1,2,3] 使用函数arrayOf()创建数组
+    val a3 = intArrayOf(1, 2, 3)
+    //[0,2,4] 使用工厂函数 创建数组
+    val b3: IntArray = IntArray(3) { i -> (i * 2) }
+
+    //读取数组内容
+    println(a3[0])    // 输出结果：1
+    println(b3)    // 输出结果：[Ljava.lang.Integer;@5e2de80c
+    println(b3[1])    // 输出结果：2
+    println(b3.size)    // 输出结果：3
+    println(b3.set(1, 1))    // kotlin.Unit
+    println(b3.get(1))    // 输出结果：1
+    b3[1] = b3[1] * 2
+    println(b3.get(1))    // 输出结果：2
 }
 
 fun parseInt(str: String): Int? {
     return null
+}
+
+fun decimalDigitValue(c: Char): Int {
+    if (c !in '0'..'9') {
+        throw IllegalArgumentException("Out of range")
+    }
+    return c.toInt() - '0'.toInt() // 显式转换为数字
 }
